2e57ba26a9d19a7aec11ee201bc04368aa5e8da5
Goldy <77524577+cooky922@users.noreply.github.com>
2025-05-04 18:23:24 +0800

    delete

--- src/MatrixException.java
+++
@@ -1,7 +0,0 @@
-package CCC102.FinalProject;
-
-public class MatrixException extends RuntimeException {
-    public MatrixException(String message) {
-        super(message);
-    }
-}
--- src/VectorException.java
+++
@@ -1,7 +0,0 @@
-package CCC102.FinalProject;
-
-public class VectorException extends RuntimeException {
-    public VectorException(String message) {
-        super(message);
-    }
-}
