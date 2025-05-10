2d47c7cb9d9f2a617f875cd887dfb1293aee1e88
Goldy <77524577+cooky922@users.noreply.github.com>
2025-05-10 13:56:11 +0800

    delete LinearSystemEnvironmentException.java

--- src/main/java/ccc102/linearizz/exceptions/LinearSystemEnvironmentException.java
+++
@@ -1,7 +0,0 @@
-package ccc102.linearizz.exceptions;
-
-public class LinearSystemEnvironmentException extends RuntimeException {
-    public LinearSystemEnvironmentException(String message) {
-        super(message);
-    }
-}
