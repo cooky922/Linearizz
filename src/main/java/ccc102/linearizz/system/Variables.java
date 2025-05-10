04fa69878f4f35d00bb0eb23b934d2b5708bcda6
Goldy <77524577+cooky922@users.noreply.github.com>
2025-05-10 13:57:41 +0800

    Create Variables.java

---
+++ src/main/java/ccc102/linearizz/system/Variables.java
@@ -0,0 +1,77 @@
+package ccc102.linearizz.system;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import ccc102.linearizz.exceptions.VariableException;
+
+public class Variables {
+    // each name is mapped to its index (the order of variables)
+    private Map<String, Integer> variableSet = new HashMap<>();
+
+    // get variable names in set
+    public Set<String> getNameSet() {
+        return variableSet.keySet();
+    }
+
+    // get variable names in order
+    public String[] getNames() {
+        String[] result = new String[variableSet.size()];
+        for (Map.Entry<String, Integer> entry : variableSet.entrySet())
+            result[entry.getValue().intValue()] = entry.getKey();
+        return result;
+    }
+
+    // clears all variable names
+    public void clear() {
+        variableSet.clear();
+    }
+
+    /// queries
+    public boolean isEmpty() {
+        return variableSet.isEmpty();
+    }
+
+    public int count() {
+        return variableSet.size();
+    }
+
+    public boolean containsName(String name) {
+        return variableSet.containsKey(name);
+    }
+
+    public void add(String name) throws VariableException {
+        if (!isValidName(name))
+            throw new VariableException(VariableException.Kind.InvalidName);
+        final int index = variableSet.size();
+        if (variableSet.containsKey(name))
+            throw new VariableException(VariableException.Kind.AlreadyExisted);
+        variableSet.put(name, index);
+    }
+
+    public void addAll(String[] c) throws VariableException {
+        for (String name : c)
+            add(name);
+    }
+
+    public void addAll(Collection<? extends String> c) throws VariableException {
+        for (String name : c)
+            add(name);
+    }
+
+    public void addAll(Variables other) throws VariableException {
+        addAll(other.getNameSet());
+    }
+
+    /// HELPERS ...........
+    // checks whether the variable name is valid
+    public static boolean isValidName(String name) {
+        if (!Character.isLetter(name.charAt(0)))
+            return false;
+        for (int i = 1; i < name.length(); ++i)
+            if (!Character.isDigit(name.charAt(i)))
+                return false;
+        return true;
+    }
+}
